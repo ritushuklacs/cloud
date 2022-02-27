ecb8298b28ec1cb6702c2535490e9a30ee373c14
ritushuklacs <96038195+ritushuklacs@users.noreply.github.com>
2022-02-27 09:53:43 +0000

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -21,6 +21,14 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_public_ip" "example" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+
+}
+
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
   location            = azurerm_resource_group.example.location
@@ -30,7 +38,7 @@ resource "azurerm_network_interface" "example" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address= azurerm_network_interface.example.id
+    public_ip_address_id= azurerm_public_ip.example.id
   }
 }
 resource "azurerm_windows_virtual_machine" "example" {
