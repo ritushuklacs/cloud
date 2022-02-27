151d9c01636975847a5d8f9c8d6118b55949afcf
ritushuklacs <96038195+ritushuklacs@users.noreply.github.com>
2022-02-27 09:51:06 +0000

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -16,7 +16,7 @@ resource "azurerm_virtual_network" "example" {
 
 resource "azurerm_subnet" "example" {
   name                 = "example-subnet"
-  resource_group_name  = data.azurerm_public_ip.example.resource_group_name
+  resource_group_name = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
 }
